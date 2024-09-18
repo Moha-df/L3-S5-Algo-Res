44497d76e60303623debbe8827d67fba39fd0aea
Mohamed DeFranceschi <mohamed.de-franceschi@etu.unistra.fr>
2024-09-18 13:55:37 +0200

    1.1 test done

--- 1-udp/1.1-emetteur/sender-udp.c
+++ 1-udp/1.1-emetteur/sender-udp.c
@@ -35,6 +35,37 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
         usage(argv[0]);
+        
+    const char *ip_addr = argv[1]; // Adresse IP de destination
+    const char *port = argv[2];    // Port de destination
+
+    //  Configuration de hints pour UDP
+    struct addrinfo *res, *p, hints = {0};
+    //struct addrinfo *res, hints = {0};
+
+    hints.ai_family = AF_UNSPEC;    // IPv4 ou IPv6
+    hints.ai_socktype = SOCK_DGRAM; // mode paquet
+    hints.ai_protocol = IPPROTO_UDP; // Protocole UDP
+
+    //  Obtenir les informations d'adresse de l'hôte
+    CHKA(getaddrinfo(ip_addr, port, &hints, &res));
+
+    //  Créer un socket UDP
+    int sockfd = -1;
+    while (p = res, p != NULL && sockfd == -1) {
+        CHK((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)));
+        p = p->ai_next;
+    }
+    //int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    //CHK(sockfd);
+
+    //  Préparer le message et envoyer
+    const char *message = "hello world";
+    CHK(sendto(sockfd, message, strlen(message), 0, p->ai_addr, p->ai_addrlen));
+    //CHK(sendto(sockfd, message, strlen(message), 0, res->ai_addr, res->ai_addrlen));
+
+    // Free la mémoire
+    freeaddrinfo(res);
 
     return 0;
 }
