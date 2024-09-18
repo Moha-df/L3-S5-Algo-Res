f916fd693db54d716d9e0df74f12d4912eddb725
Mohamed DeFranceschi <mohamed.de-franceschi@etu.unistra.fr>
2024-09-18 15:31:33 +0200

    1.3 test marche pas

--- 1-udp/1.3-transmission-continue/receiver-udp.c
+++ 1-udp/1.3-transmission-continue/receiver-udp.c
@@ -34,7 +34,16 @@ noreturn void usage(const char *msg)
 
 void copie(int src, int dst)
 {
-    return;
+    char buf[1024];
+    ssize_t n;
+    while ((n = read(src, buf, sizeof(buf))) > 0) {
+        write(dst, buf, n);
+    }
+    if (n < 0) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
 }
 
 void quit(int signo)
@@ -47,6 +56,43 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
         usage(argv[0]);
+    const char *port = argv[1];
+
+    // Préparation de l'adresse locale
+    struct addrinfo hints ={0}, *res;
+    hints.ai_family = AF_INET6;      // Accepte IPv4 et IPv6
+    hints.ai_socktype = SOCK_DGRAM;   // Socket UDP
+    hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;      // Ecouter sur toutes les interfaces
+
+    // Obtenir les informations d'adresse locale
+    CHKA(getaddrinfo(NULL, port, &hints, &res));
+
+    // Création du socket
+    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    CHK(sockfd);
+
+    // Configuration du socket pour accepter IPv4 et IPv6
+    int value = 0;
+    CHK(setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, &value, sizeof(value)));
+
+    // Association du socket à l'adresse et au port local
+    CHK(bind(sockfd, res->ai_addr, res->ai_addrlen));
+
+    // Gestion du signal SIGTERM
+    struct sigaction sa;
+    sa.sa_handler = quit;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    CHK(sigaction(SIGTERM, &sa, NULL));
+
+    // Réception des données
+    for (;;) {
+        copie(sockfd, STDOUT_FILENO);
+    }
+
+    // Libérer la mémoire de l'adresse
+    freeaddrinfo(res);
+    close(sockfd);
 
     return 0;
 }
