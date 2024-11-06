143afcb6ddf2d61ff8af913fb19b7febdda9c1be
Your Name <you@example.com>
2024-11-06 17:11:36 +0100

    correction tp3

--- 3-bit-alterne/receiver-udp.c
+++ 3-bit-alterne/receiver-udp.c
@@ -27,11 +27,43 @@
         }                                                                      \
     } while (0)
 
+#define BUFSIZE 1024 // taille max (en octets) pour les données dans un message
+#define N 2          // modulo pour numéroter les messages
+
+// format des messages
+struct message {
+    uint8_t seq;        // numéro de séquence du message
+    char data[BUFSIZE]; // données contenues dans le message
+};
 
 noreturn void usage(const char *msg)
 {
     fprintf(stderr, "usage: %s port_local\n", msg);
     exit(EXIT_FAILURE);
+}
+
+void recv_loop(int socket)
+{
+    ssize_t n;
+    uint8_t ma = 0; // message attendu
+    struct message m;
+
+    // émetteur peut utiliser une adresse IPv4 ou IPv6
+    // struct sockaddr_storage est assez grande pour stocker tout type d'adresse
+    struct sockaddr_storage ss;
+    struct sockaddr *s = (struct sockaddr *)&ss;
+    socklen_t addrlen = sizeof ss;
+
+    while ((n = recvfrom(socket, &m, sizeof m, 0, s, &addrlen)) > 0) {
+        if (m.seq == ma) { // on a reçu le message qu'on attendait
+            CHK(write(STDOUT_FILENO, m.data, n - 1));
+            ma = (ma + 1) % N;
+        }
+
+        // on envoie uniquement le num. de séquence du dernier message bien reçu
+        CHK(sendto(socket, &m, 1, 0, s, addrlen));
+    }
+    CHK(n);
 }
 
 void quit(int signo)
@@ -44,6 +76,38 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
         usage(argv[0]);
+
+    // quitte correctement le programme si SIGTERM
+    struct sigaction sa;
+    sa.sa_handler = quit;
+    sa.sa_flags = 0;
+    CHK(sigemptyset(&sa.sa_mask));
+    CHK(sigaction(SIGTERM, &sa, NULL));
+
+    // création d'un socket IPv6 qui accepte les communications IPv4
+    int sockfd, value = 0;
+    CHK(sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP));
+    CHK(setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, &value, sizeof value));
+
+    // structure d'adresse avec les paramètres locaux
+    // (toutes les adresses IPv6 du poste + port passé en argument)
+    // getaddrinfo fait ici une simple conversion
+    // (pas de requêtes DNS ou de recherche dans les fichiers locaux)
+    struct addrinfo *local, hints = {0};
+    hints.ai_family = AF_INET6;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
+    CHKA(getaddrinfo(NULL, argv[1], &hints, &local));
+
+    // association socket <-> adresse/port
+    CHK(bind(sockfd, local->ai_addr, local->ai_addrlen));
+    freeaddrinfo(local);
+
+    // réception algo bit alterné
+    recv_loop(sockfd);
+
+    // fermeture socket
+    CHK(close(sockfd));
 
     return 0;
 }
--- 3-bit-alterne/sender-udp.c
+++ 3-bit-alterne/sender-udp.c
@@ -25,6 +25,17 @@
         }                                                                      \
     } while (0)
 
+// durée (en sec.) du temporisateur pour la retransmission (RTO)
+#define TIMEOUT 2
+
+#define BUFSIZE 1024 // taille max (en octets) pour les données dans un message
+#define N 2          // modulo pour numéroter les messages
+
+// format des messages
+struct message {
+    uint8_t seq;        // numéro de séquence du message
+    char data[BUFSIZE]; // données contenues dans le message
+};
 
 noreturn void usage(const char *msg)
 {
@@ -32,10 +43,80 @@ noreturn void usage(const char *msg)
     exit(EXIT_FAILURE);
 }
 
+void send_loop(int socket)
+{
+    ssize_t n;
+    struct message m;
+
+    // on commence par envoyer le message 0 (cf. ligne 56)
+    m.seq = 1;
+
+    while ((n = read(STDIN_FILENO, m.data, BUFSIZE)) > 0) {
+        // envoi des données
+        m.seq = (m.seq + 1) % 2;
+        CHK(send(socket, &m, 1 + n, 0));
+
+        // attente (bloquante) ACK du récepteur
+        ssize_t r;
+        struct message tmp;
+
+        // la condition à droite du OU est utile si la ligne est full-duplex,
+        // ou en présence d'erreur sur contenu (cf. TD)
+        while (((r = recv(socket, &tmp, sizeof tmp, 0)) == -1 &&
+                errno == EAGAIN) ||
+               tmp.seq != m.seq)
+            // retransmission !
+            CHK(send(socket, &m, 1 + n, 0));
+        CHK(r);
+    }
+    CHK(n);
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 4)
         usage(argv[0]);
+
+    // création d'un socket IPv6 qui accepte les communications IPv4
+    int sockfd, value = 0;
+    CHK(sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP));
+    CHK(setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, &value, sizeof value));
+
+    // structure d'adresse avec les paramètres locaux
+    // (toutes les adresses IPv6 du poste + port passé en argument)
+    // getaddrinfo fait ici une simple conversion
+    // (pas de requêtes DNS ou de recherche dans les fichiers locaux)
+    struct addrinfo *local, hints = {0};
+    hints.ai_family = AF_INET6;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
+    CHKA(getaddrinfo(NULL, argv[1], &hints, &local));
+
+    // association socket <-> adresse/port
+    CHK(bind(sockfd, local->ai_addr, local->ai_addrlen));
+    freeaddrinfo(local);
+
+    // définition du temporisateur sur la non réception de données sur le socket
+    struct timeval tv = {TIMEOUT, 0};
+    CHK(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof tv));
+
+    // paramètres du récepteur (medium ici)
+    struct addrinfo *dest;
+    hints.ai_family = AF_UNSPEC; // argv[2] pourra être une adresse IPv4 ou IPv6
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV;
+    CHKA(getaddrinfo(argv[2], argv[3], &hints, &dest));
+
+    // privatise le socket pour une communication exclusive avec le medium
+    // toutes les autres sources ou destinations seront filtrées par le SE
+    CHK(connect(sockfd, dest->ai_addr, dest->ai_addrlen));
+    freeaddrinfo(dest);
+
+    // envoi algo bit alterné
+    send_loop(sockfd);
+
+    // fermeture socket
+    CHK(close(sockfd));
 
     return 0;
 }
